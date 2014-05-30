feebe991bdf454d008085b6a99ee3dcbf57f1ced
ruby-practitioner <ruby-practitioner@users.noreply.github.com>
2014-05-31 00:23:19 +0800

    initial commit; create user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyNote::Application.config.secret_key_base = '827be8c58bf0cb02b9b6e8bc4cc66e202563e29c8aa011a62ba601e17bc03f736abbc5298ef5108cd29ccde8b9e0f2bba7b13895899b45b460bd714fa9fc33aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HappyNote::Application.config.session_store :cookie_store, key: '_happy-note_session'
